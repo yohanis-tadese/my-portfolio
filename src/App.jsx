82ff1fd02d9763c42fe09fd20b4316d2805be06d
Jhontadese <yohanistadese06@gmail.com>
2024-05-01 05:29:38 +0300

    Update user interface design of students like see result and mony more

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import Homepage from "./Components/homePage/Homepage";
-import Navbar from "./Components/navbar/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import Portfolio from "./Components/portfolio/Portfolio";
 import SideBar from "./Components/sideBar/SideBar";
 import MySkill from "./Components/skill/MySkill";
