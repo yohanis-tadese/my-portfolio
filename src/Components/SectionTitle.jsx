a4e2cc4d31df9f8444f7228f1a327ea6d53b6245
Jhontadese <yohanistadese06@gmail.com>
2024-05-03 22:02:18 +0300

    commit to git to add project

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -1,6 +1,8 @@
+import "./title.scss";
+
 const SectionTitle = ({ title }) => {
   return (
-    <div className="font-titleFont text-2xl font-semibold flex items-center">
+    <div className="title-of-any font-titleFont text-2xl font-semibold flex items-center">
       {title}
       <span className="hidden md:inline-flex md:w-60 lgl:w-72 h-[.5px] bg-gray-700 ml-6"></span>
     </div>
